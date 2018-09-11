c3619c41a15a5038c6669e2bc833a6d2d18aadb5
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-09-11 21:32:01 +0200

    Convert internal keybindings

--- mfpfdzfppffzy/utils.py
+++ mfpfdzfppffzy/utils.py
@@ -1,6 +1,32 @@
 from subprocess import run
+import re
 
 PROG_NAME = 'MPD'
+MFP_BIND_RE = re.compile(r'^mfp\((.+)\)$')
+
+
+class KeyBindings(dict):
+    """
+    Subclass of a dict whose string representation conforms to fzf's
+    keybinding syntax.
+    """
+
+    def __init__(self, fifo):
+        self.fifo = fifo
+        super().__init__()
+
+    def __setitem__(self, key, value):
+        """Adapt keybinds if they are wrapped in mfp()."""
+        match = MFP_BIND_RE.match(value)
+        if match:
+            self[key] = 'execute#echo {} {{}} > {}#'.format(
+                match.group(1), self.fifo)
+        else:
+            self[key] = value
+
+    def __str__(self):
+        pairs = (':'.join(t) for t in self.items())
+        return '--bind={}'.format(','.join(pairs))
 
 
 def notify(msg):
