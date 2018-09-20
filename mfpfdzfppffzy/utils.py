eae969cb9ba11583b984e55253bb0924f3001004
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-09-20 22:03:09 +0200

    Add tests for KeyBindings

--- mfpfdzfppffzy/utils.py
+++ mfpfdzfppffzy/utils.py
@@ -1,7 +1,7 @@
 import re
 
 PROG_NAME = 'MPD'
-MFP_BIND_RE = re.compile(r'^mfp\((.+)\)$')
+MFP_BIND_RE = re.compile(r'^mfp\((.*)\)$', flags=re.DOTALL)
 
 
 class KeyBindings(dict):
@@ -12,26 +12,46 @@ class KeyBindings(dict):
 
     def __init__(self, fifo):
         self.fifo = fifo
+        self.custom_dict = {}
+        self.cmd_temp = 'echo {} {{}} > ' + self.fifo
+        self.exec_temp = 'execute#{}#'
         super().__init__()
 
     def __setitem__(self, key, value):
         """Adapt keybinds if they are wrapped in mfp()."""
+        # do nothing if this is an empty key or empty command
+        if not value.replace('&&', '').strip() or not key.strip():
+            return
+
         match = MFP_BIND_RE.match(value)
         if match:
-            self.__dict__[key] = 'execute#echo {} {{}} > {}#'.format(
-                match.group(1), self.fifo)
-        else:
-            self.__dict__[key] = value
+            mfp_cmd = match.group(1)
+
+            if '&&' in mfp_cmd:
+                value = self.get_multi_cmd(mfp_cmd)
+            else:
+                value = self.cmd_temp.format(mfp_cmd)
+
+        self.custom_dict[key] = self.exec_temp.format(value)
+
+    def get_multi_cmd(self, s):
+        """
+        Create a command for execute() made of multiple commands in s separated
+        by '&&'.
+        """
+        cmds = [s.strip() for s in s.split('&&')]
+        cmds = map(lambda x: self.cmd_temp.format(x), cmds)
+        return ' && '.join(cmds)
+
+    def items(self):
+        return self.custom_dict.items()
 
     def __getitem__(self, key):
-        return self.__dict__[key]
+        return self.custom_dict[key]
 
     def __str__(self):
         pairs = (':'.join(t) for t in self.items())
         return '--bind={}'.format(','.join(pairs))
-
-    def items(self):
-        return self.__dict__.items()
 
 
 def coroutine(f):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,23 @@
 from mfpfdzfppffzy.utils import KeyBindings
+from hypothesis import given
+from hypothesis.strategies import text
 
-
-def test_key_bindings():
-    fifo = '/tmp/bla'
+@given(text().filter(lambda x: '&&' not in x),
+       text().filter(lambda x: '&&' not in x))
+def test_key_bindings(mfp_cmd1, mfp_cmd2):
+    fifo = '/some/path/somewhere'
     kb = KeyBindings(fifo)
 
-    keysym = 'ctrl-p'
-    mfp_cmd = 'do this'
-    kb[keysym] = 'mfp({})'.format(mfp_cmd)
-    assert '{}:execute#echo {} {{}} > {}#'.format(
-        keysym, mfp_cmd, fifo) in str(kb)
+    # test conversion of single commands
+    kb['ctrl-a'] = 'mfp({})'.format(mfp_cmd1)
+    kb['ctrl-b'] = 'mfp({})'.format(mfp_cmd2)
+
+    assert 'ctrl-a:execute#echo {} {{}} > {}#'.format(mfp_cmd1, fifo) in str(kb)
+    assert 'ctrl-b:execute#echo {} {{}} > {}#'.format(mfp_cmd2, fifo) in str(kb)
+
+    # test conversion of multiple commands
+    kb = KeyBindings(fifo)
+    mfp_chained = ' && '.join([mfp_cmd1, mfp_cmd2])
+    kb['ctrl-c'] = 'mfp({})'.format(mfp_chained)
+    assert 'ctrl-c:execute#echo {0} {{}} > {2} && echo {1} {{}} > {2}#'.format(
+        mfp_cmd1.strip(), mfp_cmd2.strip(), fifo) in str(kb)
