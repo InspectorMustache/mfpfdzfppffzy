e5b9c58d55f356dfefeeade91885014532bebc66
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-12-20 22:45:04 +0100

    Simplify KeyBindings class

--- mfpfdzfppffzy/bind.py
+++ mfpfdzfppffzy/bind.py
@@ -1,85 +1,76 @@
 import re
 from .utils import coroutine
 
-MFP_KB_RE = re.compile(r'^mfp\((.*)\)$', flags=re.DOTALL)
-MFP_BIND_RE = re.compile(r'(?:^|,)([^:]+):(mfp\([^\)]*\))')
+MFP_BIND_RE = re.compile(r'^(.+?):mfp\((.*)\)$', flags=re.DOTALL)
 
 
 class KeyBindings():
     """
-    Subclass of a dict whose string representation conforms to fzf's
-    keybinding syntax.
+    An object that's represented as a tuple which can be passed to a subprocess
+    method as a command line parameter.
     """
 
     def __init__(self, args, fifo=None):
-        # base_args is an initial bind argument that can be passed as is to fzf
         self.fifo = fifo
-        self.bind_dict = {}
+        self.bind_list = []
         self.bind_tuple = None
         self.cmd_temp = 'echo {} > ' + self.fifo
         self.exec_temp = 'execute#{}#'
-        self.populate_bind_tuple(args)
-        super().__init__()
+        self.populate_bind_list(args)
 
-    def populate_bind_tuple(self, bind_str):
+    def populate_bind_list(self, binds):
         """
-        Parse the bind string and use it to populate the bind dict with fzf
+        Parse the bind string and use it to populate bind_list with fzf
         commands.
         """
         processor = self.process_bind()
-        for b in re.split(r'(?<!\\),', bind_str):
+        for b in binds:
             processor.send(b)
 
         processor.send(None)
         processor.close()
-        # use the populated bind_dict to create bind_tuple
-        pairs = tuple(':'.join(t) for t in self.bind_dict.items())
 
-        if pairs:
-            self.bind_tuple = ('--bind={}'.format(','.join(pairs)), )
+        # use the populated bind_list to create bind_tuple
+        if self.bind_list:
+            self.bind_tuple = ('--bind={}'.format(','.join(self.bind_list)), )
         else:
             self.bind_tuple = ()
 
     @coroutine
     def process_bind(self):
         """
-        Yield a binding and put it into the bind dict as a command that fzf can
+        Yield a binding and put it into bind_list as a command that fzf can
         understand.
         """
-        processor = self.process_match()
+        processor = self.process_mfp()
         while True:
             bind_str = yield
             if bind_str is None:
                 processor.close()
                 continue
 
-            match = MFP_BIND_RE.match(bind_str)
-            if match:
-                processor.send(match)
+            if MFP_BIND_RE.match(bind_str):
+                processor.send(bind_str)
             else:
-                match = bind_str.split(':', maxsplit=1)
-                try:
-                    self.bind_dict[match[0]] = match[1]
-                except IndexError:
-                    # if it's not a valid keybinding, do nothing
-                    continue
+                self.bind_list.append(bind_str)
 
     @coroutine
-    def process_match(self):
+    def process_mfp(self):
         """
-        Yield a match group and inspect its command. If it's a chained command,
+        Yield a bind string and inspect its command. If it's a chained command,
         adapt it accordingly.
         """
         while True:
-            match = yield
-            cmd = match.group(2)
-            cmd = MFP_KB_RE.match(cmd).group(1)
+            bind_str = yield
+            key, cmd = MFP_BIND_RE.match(bind_str).groups()
+
             if '&&' in cmd:
                 cmd = self.get_multi_cmd(cmd)
             else:
                 cmd = self.cmd_temp.format(cmd)
 
-            self.bind_dict[match.group(1)] = self.exec_temp.format(cmd)
+            cmd = self.exec_temp.format(cmd)
+            self.bind_list.append('{}:{}'.format(key, cmd))
 
     def get_multi_cmd(self, cmd_str):
         """
@@ -89,16 +80,6 @@ class KeyBindings():
         cmds = [s.strip() for s in cmd_str.split('&&')]
         cmds = map(lambda x: self.cmd_temp.format(x), cmds)
         return ' && '.join(cmds)
-
-    def parse_bind_args(self, args):
-        """
-        Parse provided args to the bind command and populate the dict with mfp
-        specific commands. Return the rest of the string which can be directly
-        passed to fzf.
-        """
-        for m in MFP_BIND_RE.finditer(args):
-            self[m.group(1)] = m.group(2)
-        return MFP_BIND_RE.sub('', args)
 
     def __getitem__(self, key):
         return self.bind_tuple[key]
--- tests/test_bind.py
+++ tests/test_bind.py
@@ -20,7 +20,8 @@ def text_filter(x):
 def test_key_bindings(mfp_cmd1, mfp_cmd2):
     # I trust that tempfile.mktemp doesn't create files with quotes in them...?
     fifo = '/some/path with a a space/somewhere'
-    fzf_bind_args = 'ctrl-a:mfp(y-1),ctrl-b:x-1,ctrl-c:mfp(y-2),ctrl-d:x-2'
+    fzf_bind_args = ['ctrl-a:mfp(y-1)', 'ctrl-b:x-1',
+                     'ctrl-c:mfp(y-2)', 'ctrl-d:x-2']
     kb = KeyBindings(fzf_bind_args, fifo=fifo)
     # {} should be addable by the user and not cause any problems
     mfp_cmd1 += ' {}'
@@ -29,6 +30,7 @@ def test_key_bindings(mfp_cmd1, mfp_cmd2):
     # element
     assert len(kb) == 1
     assert type(*kb) is str
+    assert kb[0][0:7] == '--bind='
 
     # test keybinds created by initiation
     assert 'ctrl-b:x-1' in kb[0]
@@ -37,8 +39,8 @@ def test_key_bindings(mfp_cmd1, mfp_cmd2):
     assert get_kb_str('ctrl-c', mfp_cmd='y-2', fifo=fifo) in kb[0]
 
     # test conversion of single commands
-    fzf_bind_args += ',ctrl-e:mfp({})'.format(mfp_cmd1)
-    fzf_bind_args += ',ctrl-f:mfp({})'.format(mfp_cmd2)
+    fzf_bind_args.append('ctrl-e:mfp({})'.format(mfp_cmd1))
+    fzf_bind_args.append('ctrl-f:mfp({})'.format(mfp_cmd2))
 
     kb = KeyBindings(fzf_bind_args, fifo=fifo)
     assert get_kb_str('ctrl-e', mfp_cmd=mfp_cmd1, fifo=fifo) in kb[0]
@@ -46,7 +48,7 @@ def test_key_bindings(mfp_cmd1, mfp_cmd2):
 
     # test conversion of multiple commands
     mfp_chained = ' && '.join([mfp_cmd1, mfp_cmd2])
-    fzf_bind_args += ',ctrl-g:mfp({})'.format(mfp_chained)
+    fzf_bind_args.append('ctrl-g:mfp({})'.format(mfp_chained))
     kb = KeyBindings(fzf_bind_args, fifo=fifo)
     assert 'ctrl-g:execute#echo {0} > {2} && echo {1} > {2}#'.format(
         mfp_cmd1.strip(), mfp_cmd2.strip(), fifo) in kb[0]
